ce4ac8c14884549e39649a8cf770b0e26c9211db
Articuno2002 <101946051+Articuno2002@users.noreply.github.com>
2022-03-19 23:51:09 +0530

    Add files via upload

---
+++ sec-filing-analyzer-frontend/tailwind.config.js
@@ -0,0 +1,27 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+    mode: "jit",
+    content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        fontFamily: {
+            mulish: "Mulish",
+        },
+        extend: {
+            colors: {
+                primary: "#3570B5",
+                secondary: "#6DB0FF",
+            },
+            fontFamily: {
+                sans: ["DM Sans", ...defaultTheme.fontFamily.sans],
+            },
+            container: {
+                center: true,
+            },
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
